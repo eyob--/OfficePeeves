726ae12453e952d99c0cf9652649d43040e03488
eyob-- <tse.eyob@gmail.com>
2017-02-08 12:08:32 -0500

    Sorta initial commit

---
+++ app/src/main/java/edu/mbhs/officepeeves/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.mbhs.officepeeves;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToWoes(View v){
+        Intent i = new Intent(this, WoesActivity.class);
+        startActivity(i);
+    }
+    public void submitWoes(View v){
+        String woe = ((EditText) findViewById(R.id.editGrievance)).getText().toString();
+        Intent i = new Intent(this, WoesActivity.class);
+        i.putExtra("woe", woe);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/edu/mbhs/officepeeves/WoesActivity.java
@@ -0,0 +1,13 @@
+package edu.mbhs.officepeeves;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_woes);
+    }
+}
